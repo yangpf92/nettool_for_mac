a12a77a1d95d86282fc9969f46678aaf3a63e946
yangpf <PengfeiYang92@outlook.com>
2020-06-15 23:50:04 +0800

    发送和接收tcp 基本ok

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class NetToolWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.__show_ipaddr()
         self.buttonNetConnect_state = False
+        self.send_msg = ""
 
     def __show_ipaddr(self):
         deviceinfo = DeviceInfo()
@@ -27,7 +28,17 @@ class NetToolWindow(QMainWindow, Ui_MainWindow):
 
     #添加信号与槽
     def add_signal_slot(self):
+        self.buttonSend.clicked.connect(self.__butonSend_slot)
         self.buttonNetConnect.clicked.connect(self.__buttonNetConnect_slot)
+
+    def __butonSend_slot(self):
+        if self.buttonNetConnect_state == True:
+            if self.textEditNetSend.document().isEmpty():
+                pass
+            else:
+                print(self.textEditNetSend.toPlainText())
+                self.send_msg = self.textEditNetSend.toPlainText()
+                #self.tcp_server.send_msg(self.textEditNetSend.toPlainText())
 
     def __buttonNetConnect_slot(self):
         if len(self.lineEditNetPort.text()) == 0:
@@ -40,9 +51,10 @@ class NetToolWindow(QMainWindow, Ui_MainWindow):
         if self.buttonNetConnect_state == False :
             self.buttonNetConnect.setText("断开连接")
             # 创建一个tcp线程
-            tcpserver_t = threading.Thread(target=self.__tcpserver_thread_cb, args=(
+            self.tcpserver_t = threading.Thread(target=self.__tcpserver_thread_cb, args=(
             self.comboBoxNetConnectAddr.currentText(), int(self.lineEditNetPort.text())))
-            tcpserver_t.start()
+            self.tcpserver_t.setDaemon(True)  # 设置后台，主线程退出则创建的子线程退出
+            self.tcpserver_t.start()
             #更新状态标志
             self.buttonNetConnect_state = True
         else:
--- tcpserver.py
+++ tcpserver.py
@@ -1,45 +1,61 @@
 # Author : Kelvin
 # Date : 2019/2/3 21:51
 import socketserver
-from main import *
+import threading
+import time
 import sys
-import main
 
 buffer_capcity = 1024
-
-g_nettool_window=NetToolWindow()
+global g_nettoolwindow
 
 class Mysocket(socketserver.BaseRequestHandler):
+    global g_nettoolwindow
+    def __sockRecv_thread_cb(self):
+        while True:
+            recv_msg = self.request.recv(buffer_capcity).decode("utf8")
+            if len(recv_msg) < 0:
+                self.request.close()
+                break
+            else:
+                g_nettoolwindow.textBrowserNetRecv.insertPlainText(recv_msg)
+
+
+    def __sockSend_thread_cb(self):
+        while True:
+            if len(g_nettoolwindow.send_msg) > 0:
+                self.request.send(g_nettoolwindow.send_msg.encode("utf8"))
+                g_nettoolwindow.send_msg = ""
 
     def handle(self):
         print(self.request)
         print(self.client_address)
-
+        #创建两个线程
+        self.sock_recv_t = threading.Thread(target=self.__sockRecv_thread_cb, args=())
+        self.sock_recv_t.setDaemon(True)    #设置后台，主线程退出则创建的子线程退出
+        self.sock_recv_t.start()
+        self.sock_send_t = threading.Thread(target=self.__sockSend_thread_cb, args=())
+        self.sock_send_t.setDaemon(True)  # 设置后台，主线程退出则创建的子线程退出
+        self.sock_send_t.start()
         while True:
-            #try:
-                data = self.request.recv(buffer_capcity).decode("utf8")
-                if len(data) <= 0:
-                    self.request.close()
-                    break
-                else:
-                    #print("服务器收到信息：%s" % data)
-                    g_nettool_window.textBrowserNetRecv.insertPlainText(data)
-                    self.request.send("recv hello word！".encode("utf8"))
-            #except Exception:
-            #    print("==== close")
-            #    self.request.close()
-            #    break
+            time.sleep(3)
+
+    def finish(self):
+        self.request.close()  # 关闭套接字
 
 class TcpSever():
-    def create(self, nettool_window, server_addr, port):
-        global g_nettool_window
-        g_nettool_window = nettool_window
+    def create(self, nettoolwindow, server_addr, port):
+        global g_nettoolwindow
+        g_nettoolwindow = nettoolwindow
         self.server_addr = server_addr
         self.port = port
         self.sock = socketserver.ThreadingTCPServer((self.server_addr, self.port), Mysocket)
         self.sock.allow_reuse_address = True  # 设置端口可重用
+
     def run(self):
         self.sock.serve_forever()
+
+
+
 
 if __name__ == "__main__":
     tcp_sever = TcpSever()
